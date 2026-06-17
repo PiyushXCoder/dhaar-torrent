5da71f738055335712d9f98254925fea41523292
Piyush Raj Mishra <piyushxcoder@gmail.com>
2026-06-17 17:16:27 +0530

    refactor: change format of peer id generation

--- src/helpers/peer_id.rs
+++ src/helpers/peer_id.rs
@@ -3,11 +3,16 @@ use rand::{RngExt, rng};
 pub fn generate_random_peer_id() -> [u8; 20] {
     let mut peer_id = [0u8; 20];
 
-    // Prefix: "dhar-"
-    peer_id[..5].copy_from_slice(b"dhar-");
+    let version = env!("CARGO_PKG_VERSION")
+        .split('.')
+        .take(2)
+        .collect::<Vec<_>>()
+        .join("");
+    let prefix = format!("-DH{}-", version);
+    peer_id[..prefix.len()].copy_from_slice(prefix.as_bytes());
 
     // Fill remaining 15 bytes with random bytes
-    rng().fill(&mut peer_id[5..]);
+    rng().fill(&mut peer_id[prefix.len()..]);
 
     peer_id
 }
